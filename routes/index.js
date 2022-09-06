8093f29e0d14980d4d351dd40880e8754b51e5da
Tharu <u7145170@anu.edu.au>
2022-09-06 19:11:52 +1000

    Add base layout to layout.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('catalog');
+  res.render('layout', {title: "Express"});
+  // res.redirect('catalog');
 });
 
 module.exports = router;
