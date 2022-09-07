36384a64a0d532b8972f35cd2cd94184407cc2a7
Tharu <u7145170@anu.edu.au>
2022-09-07 18:09:07 +1000

    Add genre_list page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,10 @@ const Genre = require('../models/genre');
 
 // Display list of all Genre.
 exports.genre_list = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre list');
+    Genre.find({})
+    .exec(function(err,result){
+      res.render('genre_list', {title: "Genre List", genre_list: result})
+    })
 };
 
 // Display detail page for a specific Genre.
